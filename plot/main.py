75936522f3833560144112eee3d8bc115f085e17
John Ramsden <john@ubc.ramsdenj.ca>
2024-04-25 22:10:44 -0700

    Add plot and stats scripts

--- plot/main.py
+++ plot/main.py
@@ -3,12 +3,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+SMALL_SIZE = 15
+MEDIUM_SIZE = 20
+BIGGER_SIZE = 25
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Plot data from a CSV file and save to PDF and PNG.')
     parser.add_argument('file_path', type=str, help='Path to the CSV file containing the data.')
     parser.add_argument('title', type=str, help='Title of the plot.')
     parser.add_argument('y_label', type=str, help='Label for the Y-axis.')
     parser.add_argument('output', type=str, help='Filename for outputs (without ext).')
+    parser.add_argument('--show_outliers', action='store_true', help='Toggle to show outliers on the plot')
+    parser.add_argument('--aggregate', action='store_true', help='Toggle to aggregate data before plotting')
     return parser.parse_args()
 
 def clean_data(data):
@@ -41,26 +55,37 @@ def detect_outliers(data):
     Q1 = data['timems'].quantile(0.25)
     Q3 = data['timems'].quantile(0.75)
     IQR = Q3 - Q1
-    outlier_threshold_upper = Q3 + 3.0 * IQR  # Using 3 times the IQR for stricter detection
-    outlier_threshold_lower = Q1 - 3.0 * IQR
+    outlier_threshold_upper = Q3 + 20.0 * IQR  # Using 6 times the IQR for stricter detection
+    outlier_threshold_lower = Q1 - 20.0 * IQR
     outliers = data[(data['timems'] > outlier_threshold_upper) | (data['timems'] < outlier_threshold_lower)]
     return outliers
 
 
-def plot_data(file_path, title, y_label, output):
+def plot_data(file_path, title, y_label, output, show_outliers, aggregate):
     # Load the data into a DataFrame
     data = pd.read_csv(file_path)
 
     # Detect outliers before aggregating
-    outliers = detect_outliers(data)
-
+    if show_outliers:
+        outliers = detect_outliers(data)
+        outlier_ts = outliers['timestamp'] / 1000
     # Aggregate data
-    aggregated_data = aggregate_data(data)
+    if aggregate:
+        aggregated_data = aggregate_data(data)
+        key = 'bin_time'
+        xdata = aggregated_data[key]
+        label = 'Aggregated Data'
+    else:
+        aggregated_data = data
+        key = 'timestamp'
+        xdata = aggregated_data[key] / 1000
+        label = 'Raw Data'
 
     # Plotting
     fig, ax = plt.subplots(figsize=(12, 7))
-    ax.plot(aggregated_data['bin_time'], aggregated_data['timems'], linestyle='-', label='Aggregated Data')
-    ax.scatter(outliers['timestamp'] / 1000, outliers['timems'], color='red', s=10, label='Outliers')  # Smaller dot for outliers
+    ax.plot(xdata, aggregated_data['timems'], linestyle='-', label=label)
+    if show_outliers:
+        ax.scatter(outlier_ts, outliers['timems'], color='red', s=10, label='Outliers')  # Smaller dot for outliers
 
     # Customizing the plot
     ax.set_title(title)
@@ -78,7 +103,7 @@ def plot_data(file_path, title, y_label, output):
 
 def main():
     args = parse_arguments()
-    plot_data(args.file_path, args.title, args.y_label, args.output)
+    plot_data(args.file_path, args.title, args.y_label, args.output, args.show_outliers, args.aggregate)
 
 
 if __name__ == '__main__':
---
+++ stats/main.py
@@ -0,0 +1,33 @@
+import argparse
+import pandas as pd
+from scipy.stats import gmean
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Calculate latency statistics from a CSV file.')
+    parser.add_argument('file_path', type=str, help='Path to the CSV file containing the data.')
+    return parser.parse_args()
+
+def calculate_statistics(file_path):
+    # Read data from CSV
+    df = pd.read_csv(file_path)
+
+    # Calculating the tail latency, e.g., 95th percentile
+    tail_latency = df['timems'].quantile(0.99)
+
+    # Calculating the standard deviation of latency
+    std_deviation = df['timems'].std()
+
+    # Calculating the geometric mean of latency
+    geo_mean = gmean(df['timems'])
+
+    # Print results
+    print("Tail Latency (99th Percentile): {:.2f} ms".format(tail_latency))
+    print("Standard Deviation: {:.2f} ms".format(std_deviation))
+    print("Geometric Mean: {:.2f} ms".format(geo_mean))
+
+def main():
+    args = parse_arguments()
+    calculate_statistics(args.file_path)
+
+if __name__ == '__main__':
+    main()
